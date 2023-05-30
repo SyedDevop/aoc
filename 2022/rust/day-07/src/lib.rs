d11976f44a859b81ea4f7c12994124522b138c78
syed Devop <syeds.devops007@gmail.com>
2023-05-30 19:51:54 +0530

    day-07 parser setup

--- 2022/rust/day-07/src/lib.rs
+++ 2022/rust/day-07/src/lib.rs
@@ -1,4 +1,72 @@
-pub fn process_part1(_input: &str) -> String {
+use std::dbg;
+
+use nom::branch::alt;
+use nom::bytes::complete::{is_a, tag};
+use nom::character::complete::{alpha1, newline};
+use nom::multi::separated_list1;
+use nom::sequence::separated_pair;
+use nom::*;
+
+#[derive(Debug)]
+enum Cmds<'a> {
+    Cd(Cd<'a>),
+    Ls(Vec<Files<'a>>),
+}
+
+#[derive(Debug)]
+enum Cd<'a> {
+    Root,
+    Up,
+    Down(&'a str),
+}
+
+#[derive(Debug)]
+enum Files<'a> {
+    File { size: u32, name: &'a str },
+    Dir(&'a str),
+}
+
+fn file(input: &str) -> IResult<&str, Files> {
+    let (input, (size, name)) = separated_pair(
+        nom::character::complete::u32,
+        tag(" "),
+        is_a("qwertyuiopasdfghjklzxcvbnm."),
+    )(input)?;
+    Ok((input, Files::File { size, name }))
+}
+
+fn directory(input: &str) -> IResult<&str, Files> {
+    let (input, _) = tag("dir ")(input)?;
+    let (input, name) = alpha1(input)?;
+    Ok((input, Files::Dir(name)))
+}
+
+fn ls(input: &str) -> IResult<&str, Cmds> {
+    let (input, _) = tag("$ ls")(input)?;
+    let (input, _) = newline(input)?;
+    let (input, files) = separated_list1(newline, alt((file, directory)))(input)?;
+    Ok((input, Cmds::Ls(files)))
+}
+fn cd(input: &str) -> IResult<&str, Cmds> {
+    let (input, _) = tag("$ cd ")(input)?;
+    let (input, dir) = alt((tag(".."), alpha1, tag("/")))(input)?;
+    let op = match dir {
+        "/" => Cmds::Cd(Cd::Root),
+        ".." => Cmds::Cd(Cd::Up),
+        name => Cmds::Cd(Cd::Down(name)),
+    };
+    Ok((input, op))
+}
+
+fn cmd_parser(input: &str) -> IResult<&str, Vec<Cmds>> {
+    let (input, cmd) = separated_list1(newline, alt((ls, cd)))(input)?;
+
+    Ok((input, cmd))
+}
+
+pub fn process_part1(input: &str) -> String {
+    let cmds = cmd_parser(input).unwrap();
+    dbg!(cmds);
     "".to_string()
 }
 
@@ -37,11 +105,13 @@ $ ls
         assert_eq!(process_part1(INPUT), "94853");
     }
     #[test]
+    #[ignore = "not impleted"]
     fn part1_5_works() {
         assert_eq!(process_part1(INPUT), "95437");
     }
 
     #[test]
+    #[ignore = "not impleted"]
     fn part2_works() {
         assert_eq!(process_part2(INPUT), "19");
     }

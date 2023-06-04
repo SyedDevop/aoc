b2f119ea3c598a0cfdfec9077ec87c4ad48d5072
Syed Devop <syeds.devops007@gmail.com>
2023-06-04 15:30:35 +0530

    day-08 complete

--- 2022/rust/day-08/src/lib.rs
+++ 2022/rust/day-08/src/lib.rs
@@ -1,27 +1,43 @@
-use std::{dbg, print};
+// use std::{dbg, print};
 
-fn is_visible_in_row(idx: usize, matrix: &Vec<u32>) -> bool {
+fn is_visible_in_row(idx: usize, matrix: &Vec<u32>) -> (bool, (u32, u32)) {
     let tree = matrix[idx];
-
     let mut result: Vec<bool> = vec![];
     let mut result_left: Vec<bool> = vec![];
+    let mut right_num = 0;
 
+    let mut left_num = 0;
     for right in (idx + 1)..matrix.len() {
+        right_num += 1;
         if tree > matrix[right] {
             result.push(true);
         } else {
-            for left in (0..idx).rev() {
-                if tree > matrix[left] {
-                    result_left.push(true);
-                } else {
-                    return false;
-                }
-            }
+            result.push(false);
+            break;
         }
     }
-
-    result.iter().all(|&x| x) || result_left.iter().all(|&x| x)
+    for left in (0..idx).rev() {
+        left_num += 1;
+        if tree > matrix[left] {
+            result_left.push(true);
+        } else {
+            result_left.push(false);
+            break;
+        }
+    }
+    let is_visible = result.iter().all(|&x| x) || result_left.iter().all(|&x| x);
+    (is_visible, (left_num, right_num))
 }
+
+fn is_visible_in_column(idx: usize, idy: usize, matrix: &Vec<Vec<u32>>) -> (bool, (u32, u32)) {
+    let mut new_row: Vec<u32> = vec![];
+    for row in matrix {
+        new_row.push(row[idx]);
+    }
+
+    is_visible_in_row(idy, &new_row)
+}
+
 pub fn process_part1(input: &str) -> String {
     let matrix: Vec<Vec<u32>> = input
         .lines()
@@ -31,19 +47,31 @@ pub fn process_part1(input: &str) -> String {
     let mut a = 0;
     for ya in 1..y {
         for xa in 1..x {
-            let v = is_visible_in_row(xa, &matrix[ya]);
-            dbg!(&v, &matrix[ya][xa]);
-            if v {
+            if is_visible_in_row(xa, &matrix[ya]).0 || is_visible_in_column(xa, ya, &matrix).0 {
                 a += 1;
             }
         }
     }
-    a.to_string()
+    let total = (x * 2 + 2) + (y * 2 - 2) + a;
+    total.to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
-    let _matrix = input;
-    "".to_string()
+    let matrix: Vec<Vec<u32>> = input
+        .lines()
+        .map(|i| i.chars().map(|a| a.to_digit(10).unwrap()).collect())
+        .collect();
+    let (y, x) = (matrix.len() - 1, matrix[0].len());
+
+    let mut scenic_scores = vec![];
+    for ya in 1..y {
+        for xa in 1..x {
+            let row = is_visible_in_row(xa, &matrix[ya]).1;
+            let col = is_visible_in_column(xa, ya, &matrix).1;
+            scenic_scores.push((row.0 * row.1) * (col.0 * col.1));
+        }
+    }
+    scenic_scores.iter().max().unwrap().to_string()
 }
 
 #[cfg(test)]
@@ -61,8 +89,7 @@ mod tests {
     }
 
     #[test]
-    #[ignore = "Not Implemented"]
     fn part2_works() {
-        assert_eq!(process_part2(INPUT), "24933642");
+        assert_eq!(process_part2(INPUT), "8");
     }
 }

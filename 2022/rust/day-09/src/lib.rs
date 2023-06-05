76c21bcb24f5f9fa3c04ccc347aab3b2d04a8601
Syed Devop <syeds.devops007@gmail.com>
2023-06-05 15:00:24 +0530

    day-09 complete

--- 2022/rust/day-09/src/lib.rs
+++ 2022/rust/day-09/src/lib.rs
@@ -1,6 +1,5 @@
+use ::lending_iterator::prelude::*;
 use itertools::Itertools;
-use std::collections::HashSet;
-
 use nom::{
     branch::alt,
     bytes::complete::tag,
@@ -9,6 +8,7 @@ use nom::{
     sequence::separated_pair,
     *,
 };
+use std::collections::HashSet;
 
 #[derive(Debug, Clone, Copy)]
 enum Direction {
@@ -71,77 +71,99 @@ pub fn process_part1(input: &str) -> String {
 }
 
 pub fn process_part2(input: &str) -> String {
-    let (_, moves) = parse_moves(input).unwrap();
-    let mut head = (0, 0);
-    let mut tail = (0, 0);
-    let mut tail_positions = HashSet::from([tail]);
-    let mut tail_head: Vec<(i32, i32)> = vec![];
-    for head_move in moves.iter() {
+    let (_, move_set) = parse_moves(input).unwrap();
+    let mut rope = [(0, 0); 10];
+    let mut tail_positions = HashSet::from([*rope.last().unwrap()]);
+
+    for head_move in move_set.iter() {
         match head_move {
-            Direction::Up => head.1 += 1,
-            Direction::Down => head.1 -= 1,
-            Direction::Right => head.0 += 1,
-            Direction::Left => head.0 -= 1,
+            Direction::Left => {
+                rope[0].0 -= 1;
+            }
+            Direction::Right => {
+                rope[0].0 += 1;
+            }
+            Direction::Up => {
+                rope[0].1 += 1;
+            }
+            Direction::Down => {
+                rope[0].1 -= 1;
+            }
         }
 
-        if tail_head.is_empty() {
+        let mut rope_windows = rope.windows_mut::<2>();
+        while let Some([ref mut head, ref mut tail]) = rope_windows.next() {
+            // println!("{:?}{:?}", head, tail);
             let x_range = (head.0 - 1)..=(head.0 + 1);
             let y_range = (head.1 - 1)..=(head.1 + 1);
 
-            let is_tail_connected = x_range.cartesian_product(y_range).any(|s| s == tail);
-            if !is_tail_connected {
-                let mut new_tail = Clone::clone(&head);
-                match head_move {
-                    Direction::Up => new_tail.1 -= 1,
-                    Direction::Down => new_tail.1 += 1,
-                    Direction::Right => new_tail.0 -= 1,
-                    Direction::Left => new_tail.0 += 1,
-                }
+            let tail_is_connected = x_range
+                .cartesian_product(y_range)
+                .any(|tuple| tuple == *tail);
 
-                tail_head.push(new_tail);
-            }
-        } else {
-            for i in 0..tail_head.len() {
-                if i == 0 {
+            if !tail_is_connected {
+                // println!("{last_head_move:?}");
+                // move_tail
+                // let mut new_tail = head.clone();
+                if head.0 == tail.0 {
+                    if head.1 > tail.1 {
+                        tail.1 += 1;
+                    } else {
+                        tail.1 -= 1;
+                    }
+                } else if head.1 == tail.1 {
+                    if head.0 > tail.0 {
+                        tail.0 += 1;
+                    } else {
+                        tail.0 -= 1;
+                    }
+                } else {
+                    // diagonal
+                    // let head_cross_positions = [
+                    //     (head.0 - 1, head.1),
+                    //     (head.0 + 1, head.1),
+                    //     (head.0, head.1 - 1),
+                    //     (head.0, head.1 + 1),
+                    // ];
                     let x_range = (head.0 - 1)..=(head.0 + 1);
                     let y_range = (head.1 - 1)..=(head.1 + 1);
 
-                    let is_tail_connected = x_range
+                    let head_3x3 = x_range.cartesian_product(y_range).collect::<Vec<_>>();
+
+                    let x_range = (tail.0 - 1)..=(tail.0 + 1);
+                    let y_range = (tail.1 - 1)..=(tail.1 + 1);
+
+                    let maybe_new_tail: Vec<(i32, i32)> = x_range
                         .cartesian_product(y_range)
-                        .any(|s| s == tail_head[0]);
-                    if !is_tail_connected {
-                        let mut new_tail = Clone::clone(&tail_head[0]);
-                        match head_move {
-                            Direction::Up => new_tail.1 -= 1,
-                            Direction::Down => new_tail.1 += 1,
-                            Direction::Right => new_tail.0 -= 1,
-                            Direction::Left => new_tail.0 += 1,
+                        .filter(|tuple| head_3x3.contains(tuple))
+                        .collect();
+                    match maybe_new_tail.len() {
+                        2 => {
+                            let new_head_cross_positions = [
+                                (head.0 - 1, head.1),
+                                (head.0 + 1, head.1),
+                                (head.0, head.1 - 1),
+                                (head.0, head.1 + 1),
+                            ];
+                            let next = maybe_new_tail
+                                .iter()
+                                .find(|tuple| new_head_cross_positions.contains(tuple))
+                                .unwrap();
+                            *tail = *next;
                         }
-
-                        tail_head.push(new_tail);
-                    }
+                        1 => {
+                            *tail = maybe_new_tail[0];
+                        }
+                        _ => {
+                            panic!("unknown tail length");
+                        }
+                    };
+                    // *tail = new_tail;
                 }
-
-                let x_range = (tail_head[0].0 - 1)..=(head.0 + 1);
-                let y_range = (head.1 - 1)..=(head.1 + 1);
             }
         }
 
-        let x_range = (head.0 - 1)..=(head.0 + 1);
-        let y_range = (head.1 - 1)..=(head.1 + 1);
-
-        let is_tail_connected = x_range.cartesian_product(y_range).any(|s| s == tail);
-        if !is_tail_connected {
-            let mut new_tail = Clone::clone(&head);
-            match head_move {
-                Direction::Up => new_tail.1 -= 1,
-                Direction::Down => new_tail.1 += 1,
-                Direction::Right => new_tail.0 -= 1,
-                Direction::Left => new_tail.0 += 1,
-            }
-            tail = new_tail;
-            tail_positions.insert(new_tail);
-        }
+        tail_positions.insert(*rope.last().unwrap());
     }
     tail_positions.len().to_string()
 }

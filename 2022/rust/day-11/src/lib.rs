90efee4ab373f53985894d22b7c1b72c987f9ae4
Syed Devop <syeds.devops007@gmail.com>
2023-06-07 00:18:05 +0530

    day-11 complete

--- 2022/rust/day-11/src/lib.rs
+++ 2022/rust/day-11/src/lib.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::{BTreeMap, BTreeSet},
-    dbg, todo,
-};
+use std::{collections::VecDeque, dbg, todo};
 
 use nom::{
     branch::alt,
@@ -15,7 +12,7 @@ use nom::{
 #[derive(Debug)]
 enum Value {
     Old,
-    Num(u32),
+    Num(u64),
 }
 #[derive(Debug)]
 enum Operation {
@@ -25,23 +22,55 @@ enum Operation {
 
 #[derive(Debug)]
 struct Test {
-    divisible: u32,
-    true_recp: u32,
-    false_recp: u32,
+    divisible: u64,
+    true_recp: u64,
+    false_recp: u64,
 }
 
 #[derive(Debug)]
 struct Monkey {
-    items: Vec<u32>,
+    items: VecDeque<u64>,
     opration: Operation,
     test: Test,
+    inspect_count: u64,
 }
+
+impl Monkey {
+    fn inspect(&mut self, is_releafe: bool, magic_num: u64) -> u64 {
+        self.inspect_count += 1;
+        let old_item = self.items.pop_front().unwrap();
+        let mut new_item: u64 = match &self.opration {
+            Operation::Mul(val) => match val {
+                (Value::Old, Value::Old) => old_item * old_item,
+                (Value::Old, Value::Num(num)) => old_item * num,
+                _ => panic!("No operation  to perform"),
+            },
+            Operation::Add(val) => match val {
+                (Value::Old, Value::Num(num)) => old_item + num,
+                _ => panic!("No operation  to perform"),
+            },
+        };
+        new_item %= magic_num;
+        if is_releafe {
+            new_item /= 3;
+        };
+        new_item
+    }
+    fn test(&self, num: u64) -> usize {
+        if num % self.test.divisible == 0 {
+            self.test.true_recp as usize
+        } else {
+            self.test.false_recp as usize
+        }
+    }
+}
+
 fn test(input: &str) -> IResult<&str, Test> {
-    let (input, divisible) = preceded(tag("Test: divisible by "), complete::u32)(input)?;
+    let (input, divisible) = preceded(tag("Test: divisible by "), complete::u64)(input)?;
     let (input, _) = multispace1(input)?;
-    let (input, true_recp) = preceded(tag("If true: throw to monkey "), complete::u32)(input)?;
+    let (input, true_recp) = preceded(tag("If true: throw to monkey "), complete::u64)(input)?;
     let (input, _) = multispace1(input)?;
-    let (input, false_recp) = preceded(tag("If false: throw to monkey "), complete::u32)(input)?;
+    let (input, false_recp) = preceded(tag("If false: throw to monkey "), complete::u64)(input)?;
     Ok((
         input,
         Test {
@@ -55,7 +84,7 @@ fn test(input: &str) -> IResult<&str, Test> {
 fn value(input: &str) -> IResult<&str, Value> {
     alt((
         tag("old").map(|_| Value::Old),
-        complete::u32.map(Value::Num),
+        complete::u64.map(Value::Num),
     ))(input)
 }
 fn opration(input: &str) -> IResult<&str, Operation> {
@@ -72,11 +101,11 @@ fn opration(input: &str) -> IResult<&str, Operation> {
 }
 
 fn parse_monkeys(input: &str) -> IResult<&str, Monkey> {
-    let (input, _id) = delimited(tag("Monkey "), complete::u32, tag(":"))(input)?;
+    let (input, _id) = delimited(tag("Monkey "), complete::u64, tag(":"))(input)?;
     let (input, _) = multispace1(input)?;
     let (input, items) = preceded(
         tag("Starting items: "),
-        separated_list1(tag(", "), complete::u32),
+        separated_list1(tag(", "), complete::u64),
     )(input)?;
     let (input, _) = multispace1(input)?;
     let (input, opration) = opration(input)?;
@@ -85,24 +114,67 @@ fn parse_monkeys(input: &str) -> IResult<&str, Monkey> {
     Ok((
         input,
         Monkey {
-            items,
+            items: VecDeque::from(items),
             opration,
             test,
+            inspect_count: 0,
         },
     ))
 }
 
 pub fn process_part1(input: &str) -> String {
-    let (_, monkeys) = separated_list1(tag("\n\n"), parse_monkeys)(input).unwrap();
-    let mon: BTreeSet<Vec<Monkey>> = BTreeSet::from(monkeys);
-    let mon_map = BTreeMap::from(monkeys);
-    dbg!(mon);
-    dbg!(mon_map);
-    "".to_string()
+    let (_, mut monkeys) = separated_list1(tag("\n\n"), parse_monkeys)(input).unwrap();
+
+    let magic_trick = monkeys
+        .iter()
+        .map(|monkey| monkey.test.divisible)
+        .product::<u64>();
+    for _ in 0..20 {
+        for i in 0..monkeys.len() {
+            for _ in 0..monkeys[i].items.len() {
+                let new_item = monkeys[i].inspect(true, magic_trick);
+                let pass_index = monkeys[i].test(new_item);
+                monkeys[pass_index].items.push_back(new_item);
+            }
+        }
+    }
+
+    monkeys.sort_by_key(|mon| mon.inspect_count);
+    monkeys
+        .iter()
+        .rev()
+        .take(2)
+        .map(|i| i.inspect_count)
+        .product::<u64>()
+        .to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
-    input.to_string()
+    let (_, mut monkeys) = separated_list1(tag("\n\n"), parse_monkeys)(input).unwrap();
+
+    let magic_trick = monkeys
+        .iter()
+        .map(|monkey| monkey.test.divisible)
+        .product::<u64>();
+    for _ in 0..10_000 {
+        for i in 0..monkeys.len() {
+            for _ in 0..monkeys[i].items.len() {
+                let new_item = monkeys[i].inspect(false, magic_trick);
+                let pass_index = monkeys[i].test(new_item);
+                monkeys[pass_index].items.push_back(new_item);
+            }
+        }
+    }
+
+    monkeys.sort_by_key(|mon| mon.inspect_count);
+    dbg!(&monkeys.iter().map(|i| i.inspect_count).collect::<Vec<_>>());
+    monkeys
+        .iter()
+        .rev()
+        .take(2)
+        .map(|i| i.inspect_count)
+        .product::<u64>()
+        .to_string()
 }
 
 #[cfg(test)]
@@ -143,8 +215,7 @@ Monkey 3:
     }
 
     #[test]
-    #[ignore = "reason"]
     fn part2_works() {
-        assert_eq!(process_part2(INPUT), "");
+        assert_eq!(process_part2(INPUT), "2713310158");
     }
 }

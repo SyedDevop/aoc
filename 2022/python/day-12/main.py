ded2b0d40d884a4347dbdb39f27f4c0932aa4885
syed Devop <syeds.devops007@gmail.com>
2023-06-12 21:41:39 +0530

    python day-12 part_one

--- 2022/python/day-12/main.py
+++ 2022/python/day-12/main.py
@@ -43,13 +43,8 @@ def get_neighbors(cur: Tuple[int, int], maze: Maze) -> List[Tuple[int, int]]:
     nei_list = []
     x, y = cur
     letter = maze[y][x]
-    cur_car = 0
-    if "S" == letter:
-        cur_car = ord("a")
-    elif "E" == letter:
-        cur_car = ord("z")
-    else:
-        cur_car = ord(letter)
+    cur_car = ord(letter)
+
     if y < maze.__len__() - 1 and ord(maze[y + 1][x]) <= cur_car + 1:  # DOWN
         nei_list.append((x, y + 1))
     if y > 0 and ord(maze[y - 1][x]) <= cur_car + 1:  # UP
@@ -62,9 +57,8 @@ def get_neighbors(cur: Tuple[int, int], maze: Maze) -> List[Tuple[int, int]]:
 
 
 def algorithm(grid: Grid, maze: Maze, start: Tuple[int, int], end: Tuple[int, int]):
-    count = 0
     open_set = PriorityQueue()  # type: ignore
-    open_set.put((0, count, start))
+    open_set.put((0, start))
     came_from = {}  # type: ignore
     g_score = {spot: float("inf") for row in grid for spot in row}
     g_score[start] = 0
@@ -72,15 +66,18 @@ def algorithm(grid: Grid, maze: Maze, start: Tuple[int, int], end: Tuple[int, in
     f_score[start] = h(start, end)
     open_set_hash = {start}
     while not open_set.empty():
-        current = open_set.get()[2]
+        current = open_set.get()[1]
         open_set_hash.remove(current)
 
         if current == end:
-            print(current)
-            return came_from.__len__()
-        na = get_neighbors(current, maze)
+            path = []
+            while current in came_from:
+                current = came_from[current]
+                path.append(current)
+            path.reverse()
+            return path.__len__()
 
-        for neighbor in na:
+        for neighbor in get_neighbors(current, maze):
             temp_g_score = g_score[current] + 1
 
             if temp_g_score < g_score[neighbor]:
@@ -88,12 +85,8 @@ def algorithm(grid: Grid, maze: Maze, start: Tuple[int, int], end: Tuple[int, in
                 g_score[neighbor] = temp_g_score
                 f_score[neighbor] = temp_g_score + h(neighbor, end)
                 if neighbor not in open_set_hash:
-                    # print(neighbor, open_set_hash)
-                    count += 1
-                    open_set.put((f_score[neighbor], count, neighbor))
+                    open_set.put((f_score[neighbor], neighbor))
                     open_set_hash.add(neighbor)
-    # print(came_from)
-    # print(open_set_hash)
     return 0.0
 
 
@@ -104,88 +97,44 @@ def h(p1, p2):
     return abs(x1 - x2) + abs(y1 - y2)
 
 
-def a_star(maze, start, end):
-    def heuristic(p1, p2):
-        x1, y1 = p1
-        x2, y2 = p2
-        return abs(x1 - x2) + abs(y1 - y2)
-
-    def get_neighbors_in(pos):
-        nei_list = []
-        x, y = pos
-        letter = maze[y][x]
-        cur_car = 0
-        if "S" == letter:
-            cur_car = ord("a")
-        elif "E" == letter:
-            cur_car = ord("z")
-        else:
-            cur_car = ord(letter)
-        if y < maze.__len__() - 1 and ord(maze[y + 1][x]) <= cur_car + 1:  # DOWN
-            nei_list.append((x, y + 1))
-        if y > 0 and ord(maze[y - 1][x]) <= cur_car + 1:  # UP
-            nei_list.append((x, y - 1))
-        if x < maze[y].__len__() - 1 and ord(maze[y][x + 1]) <= cur_car + 1:  # RIGHT
-            nei_list.append((x + 1, y))
-        if x > 0 and ord(maze[y][x - 1]) <= cur_car + 1:  # LEFT
-            nei_list.append((x - 1, y))
-        return nei_list
-
-    open_set = PriorityQueue()
-    open_set.put((0, start))
-    came_from = {}
-    g_score = {start: 0}
-    f_score = {start: heuristic(start, end)}
-    open_set_hash = {start}
-
-    while not open_set.empty():
-        current = open_set.get()[1]
-        open_set_hash.remove(current)
-
-        if current == end:
-            path = []
-            while current in came_from:
-                path.append(current)
-                current = came_from[current]
-            path.reverse()
-            print(path.__len__())
-            return path
-
-        for neighbor in get_neighbors_in(current):
-            tentative_g_score = g_score[current] + 1
-
-            if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
-                came_from[neighbor] = current
-                g_score[neighbor] = tentative_g_score
-                f_score[neighbor] = tentative_g_score + heuristic(neighbor, end)
-                if neighbor not in open_set_hash:
-                    open_set.put((f_score[neighbor], neighbor))
-                    open_set_hash.add(neighbor)
-
-    return []
-
-
 def part_one(data: str):
     maze: Maze = [[*i] for i in data.split("\n") if i.__len__() > 0]
     grid: Grid = []
+    start = get_start_pos(maze)
+    end = get_end_pos(maze)
 
     for y in range(maze.__len__()):
         grid.append([])
         for x in range(maze[0].__len__()):
             grid[y].append((x, y))
 
-    start = get_start_pos(maze)
-    end = get_end_pos(maze)
-
-    # print(maze)
     if start and end:
-        path = a_star(maze, start, end)
-        print(path)
-        print(algorithm(grid, maze, start, end))
+        maze[start[1]][start[0]] = "a"
+        maze[end[1]][end[0]] = "z"
+        return algorithm(grid, maze, start, end)
+    return 0
 
-    # print(get_neighbors((0, 1), maze))
 
-    return 10
+def part_two(data: str):
+    maze: Maze = [[*i] for i in data.split("\n") if i.__len__() > 0]
+    grid: Grid = []
+    end = get_start_pos(maze)
+    start = get_end_pos(maze)
+
+    for y in range(maze.__len__()):
+        grid.append([])
+        for x in range(maze[0].__len__()):
+            grid[y].append((x, y))
+
+    if start and end:
+        maze[start[1]][start[0]] = "a"
+        maze[end[1]][end[0]] = "z"
+        return algorithm(grid, maze, start, end)
+    return 0
+
+
+with open("./input.txt", "r") as file:
+    print(part_one(file.read()))
 
 
 # {Test}
@@ -194,10 +143,13 @@ abcryxxl
 accszExk
 acctuvwj
 abdefghi"""
-with open("./input.txt", "r") as file:
-    part_one(file.read())
 
 
 def test_part_one():
     ans = part_one(INPUT)
-    assert ans == 10
+    assert ans == 31
+
+
+def test_part_two():
+    ans = part_two(INPUT)
+    assert ans == 29

f8f4a632fa8e6d07b25ac44d045036e7aaf04eb1
Syed Devops <syeds.devops007@gmail.com>
2023-12-05 00:52:02 +0530

    go day04 part_two WIP

--- 2023/go/day04/main.go
+++ 2023/go/day04/main.go
@@ -22,6 +22,26 @@ func isNumeric(s string) bool {
 	return err == nil
 }
 
+func parseCards(line string) (winingNumbers []string, myNumbers string) {
+	cardsBody := strings.Split(line, ": ")
+	cards := strings.Split(cardsBody[1], " |")
+	winNumber := strings.Split(cards[0], " ")
+	myNumber := cards[1] + " "
+
+	return winNumber, myNumber
+}
+
+func cardNumWone(numbers, n string) bool {
+	num := strings.TrimSpace(n)
+	if !isNumeric(num) {
+		return false
+	}
+	if len(num) == 1 {
+		num = " " + num + " "
+	}
+	return strings.Contains(numbers, num)
+}
+
 func main() {
 	part1 := part1(parseInput(input))
 	part2 := part2(parseInput(input))
@@ -32,21 +52,11 @@ func part1(input []string) int {
 	sumRound := 0
 	for _, line := range input {
 		roungScore := 0
-		cardsBody := strings.Split(line, ": ")
 
-		cards := strings.Split(cardsBody[1], " |")
-		winNumber := strings.Split(cards[0], " ")
-		myNumber := cards[1] + " "
+		winingNumbers, myNumbers := parseCards(line)
 
-		for _, RawNum := range winNumber {
-			num := strings.TrimSpace(RawNum)
-			if !isNumeric(num) {
-				continue
-			}
-			if len(num) == 1 {
-				num = " " + num + " "
-			}
-			isMatch := strings.Contains(myNumber, num)
+		for _, RawNum := range winingNumbers {
+			isMatch := cardNumWone(myNumbers, RawNum)
 
 			if isMatch {
 				if roungScore == 0 {
@@ -62,6 +72,28 @@ func part1(input []string) int {
 }
 
 func part2(input []string) int {
+	rounds := make(map[int]int)
+
+	for carNum, cardValue := range input {
+		winingNumbers, myNumbers := parseCards(cardValue)
+		roundScore := 0
+		for _, RawNum := range winingNumbers {
+			isMatch := cardNumWone(myNumbers, RawNum)
+			if isMatch {
+				roundScore++
+			}
+		}
+		rounds[carNum+1] = roundScore
+	}
+
+	fmt.Println(rounds)
+	for index, val := range rounds {
+		for i := index + 1; i <= val; i++ {
+			rounds[i]++
+		}
+	}
+
+	fmt.Println(rounds)
 	return 0
 }
 
--- 2023/go/day04/main_test.go
+++ 2023/go/day04/main_test.go
@@ -6,32 +6,33 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestA(t *testing.T) {
-	// 	input := `Card  76: 70 94  3  1 46 48 87  5 16 74 | 52 14 22  6 24 65  4  8 42 36 66 43  9 45 93 69 51 57 19 44 81 98 77 35 79
-	// Card  77: 77  8 29 21 11 31 93 74 72 71 | 67  8 43 72 62 40 11 77 71 29 61 92 74 12 52 37 78 93 56 31 14 21 63 39 35
-	// Card  78: 66 48  5  4 63 54 91 74 76 77 | 48 54 36 95 11 61 76 52 46 65 18 67 66 63 62 19  5 74 77 64  4 42  9 91 55`
-
-	result := part1(parseInput(input))
-
-	require.Equal(t, 23235, result)
-}
-
-func BenchmarkA(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		_ = part1(parseInput(input))
-	}
-}
+// func TestA(t *testing.T) {
+// 	// 	input := `Card  76: 70 94  3  1 46 48 87  5 16 74 | 52 14 22  6 24 65  4  8 42 36 66 43  9 45 93 69 51 57 19 44 81 98 77 35 79
+// 	// Card  77: 77  8 29 21 11 31 93 74 72 71 | 67  8 43 72 62 40 11 77 71 29 61 92 74 12 52 37 78 93 56 31 14 21 63 39 35
+// 	// Card  78: 66 48  5  4 63 54 91 74 76 77 | 48 54 36 95 11 61 76 52 46 65 18 67 66 63 62 19  5 74 77 64  4 42  9 91 55`
+//
+// 	result := part1(parseInput(input))
+//
+// 	require.Equal(t, 23235, result)
+// }
+//
+// func BenchmarkA(b *testing.B) {
+// 	for i := 0; i < b.N; i++ {
+// 		_ = part1(parseInput(input))
+// 	}
+// }
 
 func TestB(t *testing.T) {
-	input := `Game 2: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-	Game 3: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-	Game 4: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-	Game 5: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-	Game 6: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
+	input := `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`
 
 	result := part2(parseInput(input))
 
-	require.Equal(t, 0, result)
+	require.Equal(t, 30, result)
 }
 
 func BenchmarkB(b *testing.B) {
